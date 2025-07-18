a6f9b634d371c20b98a71aeb33933c02ef347216
DiegoFatVega <fatvegadiego@gmail.com>
2025-07-18 14:13:46 +0200

    Implement palindrome checking functionality

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
 /* Palidroma
 Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma */
+
+//ricevo la parola come input e la salvo in una variabile
+const word = prompt('inserisci una parola: ')
+
+//creo una funzione che  inverta la parola e la salvo in un altra variabile
+
+function isPalindrome (word){
+    let reversedWord = '';
+    for(let i = word.length - 1; i >= 0; i --){
+        const char = word[i];
+        reversedWord = reversedWord + char;
+    }
+    return reversedWord;
+}
+
+const reversedWord = isPalindrome(word)
+console.log(reversedWord);
+//faccio il confronto tra la prima variabile e la seconda con la prima parola pero invertita
+
+//se sono ugali, stampo il messaggio "è palindroma", altrimenti "non è palindroma"
